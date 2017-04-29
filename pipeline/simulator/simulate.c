e2ddb14a674d34ecc6c04e619ad8171b6580bd78
k0079898 <k0079898@gmail.com>
2017-04-29 19:47:30 +0800

    fix the branch stall bug

--- pipeline/simulator/simulate.c
+++ pipeline/simulator/simulate.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "piperpt.h"
 #include "memory.h"
 #include "regfile.h"
@@ -12,17 +13,41 @@ void initialSetting()
   initREG();
 }
 
+void detectSimpleEnd()
+{
+    if( strcmp(prevDM_WB.instName, "HALT")==0 ) halt=1;
+}
+
 int main()
 {
-  //Initial the simlator
-  unsigned int cycles;
-  initialSetting();
-  readBin();
-  writeMem();
-  cycles = 0;
-  initPipeReg();
-  writeInitSnapshot();
-  //Do something
-  closeFile();
-  return 0;
+    //Initial the simlator
+    unsigned int cycles;
+    initialSetting();
+    readBin();
+    writeMem();
+    cycles = 0;
+    initPipeReg();
+    writeToRegZero = 0;
+    overwriteHILO = 0;
+    memAddOverflow = 0;
+    dataMisaligned = 0;
+    numberOverflow = 0;
+    while(halt!=1)
+    {
+        writeZeroError(cycles);
+        writeError(cycles);
+        writeSnapshotREG(cycles);
+        WBprocess();
+        DMprocess();
+        EXprocess();
+        IDprocess();
+        IFprocess();
+        writeSnapshotPipe(cycles);
+        if(flush!=1 && stall!=1) PC = PC + 4;
+        else if(flush) PC = jumpAddress;
+        cycles++;
+        detectSimpleEnd();
+    }
+    closeFile();
+    return 0;
 }
